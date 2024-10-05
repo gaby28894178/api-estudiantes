b55cb531970f605e1cd0386e05bc0628d7e52330
gaby28894178 <gabgabrielligabriel@gmail.com>
2024-10-05 16:59:35 -0300

    secundo commitesd

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,6 +1,17 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, { logging: false })
+const sequelize = new Sequelize(process.env.DATABASE_URL, { 
+    logging: false,
+    dialectOptions: {
+        ssl: {
+          require: true, // Requerir SSL
+          rejectUnauthorized: false // Desactivar la verificación de certificados si es necesario
+        }
+      }
+
+
+
+})
 
 module.exports = sequelize;
